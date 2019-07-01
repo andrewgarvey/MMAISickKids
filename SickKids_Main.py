5f271c1219ca1278e29ca8c976582f325fe45acf
andrewgarvey <andrewtgarvey@gmail.com>
2019-07-01 13:13:45 -0400

    update gitignore, add main file

---
+++ SickKids_Main.py
@@ -0,0 +1,21 @@
+"""
+Author: Andrew Garvey
+Client: SickKids
+
+Main File:
+
+-Imports Packages
+-Imports all other functions required from other files
+    Exploration key results
+    Data Cleaning
+    Data Modeling
+
+-Imports target Data
+    explores/cleans/models
+
+Outputs
+
+
+
+
+"""
