ec9c0b7e54fb9222d9862fa7237dee0ab448f48f
andrewgarvey <andrewtgarvey@gmail.com>
2019-07-01 21:24:28 -0400

    happy for now, got things all going smoothly w linux/python all the things i wanted, files seem to import fine, mostly worried that ED data isn't present

--- Main/DI_Review_Main.py
+++ Main/DI_Review_Main.py
@@ -10,10 +10,6 @@ Purpose:
 -   Look for any trends in DI data that might be useful as it relates to staffing on a long term basis
 -   Prepare visualizations for a 1 time presentation
 """
-# Set paths
-data_path = '/home/andrew/Public/DI'
-output_path = '/home/andrew/PycharmProjects/SickKidsMMAI/Generated_Outputs/DI_Review_Output/'
-
 # Import Packages
 import os
 import numpy as np
@@ -21,17 +17,27 @@ import pandas as pd
 
 # Import Custom Functions
 from Data_Functions import test_func
-from Visualization_Functions import test
 
 # Import DI Data
-os.chdir(data_path)
+DI_2018_Q3 = pd.read_excel('/home/andrew/Public/DI/ED DI 2018 - Q3.xlsx')  # only 1 sheet
+DI_2018_Q4 = pd.read_excel('/home/andrew/Public/DI/ED DI 2018 - Q4.xlsx')
+DI_2019_Q1 = pd.read_excel('/home/andrew/Public/DI/ED DI 2019 - Q1.xlsx')
+DI_2019_Q2 = pd.read_excel('/home/andrew/Public/DI/ED DI 2019 - Q2 20190621.xlsx')
 
-
-# All results go to output path
-os.chdir(output_path)
+# All results will go to output path
+os.chdir('/home/andrew/PycharmProjects/SickKidsMMAI/Generated_Outputs/DI_Review_Output/')
 
 # ----------------------------------------------------------------------------------------------------------------------
 # Data Exploration
 
+# Just check that not everything is horribly wrong
 
-os.getcwd()
+DI_2018_Q3.describe()
+DI_2018_Q3.columns.values
+
+DI_2018_Q3.shape
+DI_2018_Q4.shape
+DI_2019_Q1.shape
+DI_2019_Q2.shape  # looks like 22 k rows ish, 21 columns
+
+# As far as DI goes, seems pretty good to me
--- Main/ED_Review_Main.py
+++ Main/ED_Review_Main.py
@@ -10,3 +10,23 @@ Purpose:
 -   Look for any trends in ED data that might be useful as it relates to staffing on a long term basis
 -   Prepare visualizations for a 1 time presentation
 """
+# Import Packages
+import os
+import numpy as np
+import pandas as pd
+
+# Import Custom Functions
+from Data_Functions import test_func
+
+# Import ED Data
+# ED_data = ???, are these guys prejoined?
+
+# All results will go to output path
+os.chdir('/home/andrew/PycharmProjects/SickKidsMMAI/Generated_Outputs/ED_Review_Output/')
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Data Exploration
+
+# Just check that not everything is horribly wrong,
+
+# um should have seperate triage based data somewhere?
