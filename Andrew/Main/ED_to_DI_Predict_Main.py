8024d02d477fc3f5818bd16b55224cb717c6b3c5
andrewgarvey <andrewtgarvey@gmail.com>
2019-08-23 12:01:27 -0400

    figured out sql, left joined to have everyone in a propertimeframe database

--- Andrew/Main/ED_to_DI_Predict_Main.py
+++ Andrew/Main/ED_to_DI_Predict_Main.py
@@ -23,6 +23,8 @@ import pandas as pd
 import matplotlib as mpl
 import datetime as dt
 import os
+from pandasql import sqldf
+
 
 #Set dir
 os.chdir('/home/andrew/PycharmProjects/SickKidsMMAI/Generated_Outputs/Output/')
@@ -33,27 +35,20 @@ DI_Clean = pd.read_csv('/home/andrew/PycharmProjects/SickKidsMMAI/Generated_Outp
 
 ED_Clean.shape
 
-# Check in on the amount of people that actually match between the two, same MRN
-All_Clean = ED_Clean.merge(DI_Clean, how='left', on = 'MRN' )
-All_Clean.shape #plenty because MRN is not distinct in either table,
-All_Clean.dtypes
-
-# Check in on the stats not being hilariously wrong particularly for the 3 key joining dates (arrived,order,discharge)
-All_Clean.isna().sum() # because we have many ED visitors who went without tests , keeping those as it is a valid result
-
 # Restrict the Joined rows to be based on order dates that are acceptable (arrived -> order -> discharge)
-# Or just no tests for that visit is ok too
-Arrived_before_Order = All_Clean['Arrived'] < All_Clean['Order Time']
-Discharge_after_Order = All_Clean['Order Time'] < All_Clean['Disch Date/Time']
-No_Tests_Performed = (All_Clean['Order Time'].isna())
+# Or just no tests for that visit is ok too, this will show up as null
+# DI Timeframe entirely encompasses ED, so if they got a test they should be here.
 
-Logical_Timing_Index = (Arrived_before_Order & Discharge_after_Order) | No_Tests_Performed
-All_Clean_Logical = All_Clean[Logical_Timing_Index]
+# Could not find a clean way to do this that wouldnt take a bunch of extra work in python, using sql
+pysqldf = lambda q: sqldf(q, globals())  # believe this imports all variables to be able to be used in sql
 
-All_Clean_Logical.shape
+All_Clean = pysqldf("SELECT * FROM ED_Clean AS e "
+                    "LEFT JOIN DI_Clean AS d " #  left join because NO tests is a valid answer to incoming patient
+                    "ON e.MRN = d.MRN "  #  same person
+                    "AND e.Arrived < d.[Order Time]"  #  arrived before order
+                    "AND e.[Disch Date/Time] > d.[Order Time]")  # discharged after order
 
 # Drop rows that we cannot possible have at the time this model aims to be used (nearly all of DI, some of ED)
 # Later we might have to attempt to quantify how paths of people work out
-
-
+All_Clean.isna().sum()
 
